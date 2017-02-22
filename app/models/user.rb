a3fc2d3dd2ad58a412220069e6b2ff93e84d4527
nerzid <nerzid@gmail.com>
2017-02-22 15:46:50 +0300

    devise controllers added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
   rolify
+  after_create :assign_default_role
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
-  validates_acceptance_of :terms_of_service, :allow_nil => false, :accept => true, :on => :create
+  validates :terms_of_service, :allow_nil => false, :acceptance => true
+
+  def assign_default_role
+    self.add_role(:freeuser)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+      :sessions => "users/sessions",
+      :registrations => "users/registrations"}
 
   get 'home/faq', to: 'home#faq', path: '/faq'
 
