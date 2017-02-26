9feb2ea1a01b6cc14566f781e976329a3b9f238c
nerzid <nerzid@gmail.com>
2017-02-27 02:14:27 +0300

    navigation bar added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+
+  def is_student?
+    current_user.has_role?(:student)
+  end
+  helper_method :is_student?
+
+  def is_teacher?
+    current_user.has_role?(:teacher)
+  end
+  helper_method :is_teacher?
+
+  def is_seller?
+    current_user.has_role?(:seller)
+  end
+  helper_method :is_seller?
+
+  def index
+
+  end
+
+  def timeline
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
       :sessions => "users/sessions",
       :registrations => "users/registrations"}
+  resources :users
 
-  get 'home/faq', to: 'home#faq', path: '/faq'
-
-  root to: 'home#faq'
+  devise_scope :user do
+    authenticated :user do
+      root "users#index"
+    end
+    unauthenticated :user do
+      root "devise/sessions#new"
+    end
+  end
 end
