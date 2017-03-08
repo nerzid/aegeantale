a742e58df023eb249a02136be91d13139f3f0812
nerzid <nerzid@gmail.com>
2017-03-08 10:07:21 +0300

    css to upload avatar is better now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
   protect_from_forgery with: :exception
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:user) << :avatar
+    devise_parameter_sanitizer.permit(:avatar)
   end
 end
